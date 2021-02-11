9fa3c9ebb66f1cd9dc25399b1db7c8e8a1d4eb6f
javier <javiermontes1997@hotmail.com>
2021-02-11 22:01:50 +0100

    instalación de prettier y creación de variables de entorno

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./routes/index');
-const cors = require('cors');
+const routes = require("./routes");
+const cors = require("cors");
+require("dotenv").config();
 
 //Config
-app.set('PORT', process.env.PORT || 3000)
+const PORT = process.env.PORT || 3000;
 
 // Middleweares
 app.use(cors());
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 
-app.listen(app.get('PORT'), () => {
-  console.log('API HL TV funcionando en el puerto ' + app.get('PORT'));
+app.listen(PORT, () => {
+  console.log("API funcionando en el puerto " + PORT);
 });
